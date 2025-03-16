9ee169846c4d3ee4ee7c1dd07b10610aad03e25c
ahmadshajee <145528435+ahmadshajee@users.noreply.github.com>
2025-03-17 02:21:27 +0530

    Delete MY WB directory

--- MY WB/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting local server for Ahmad Shajee Zubairy's Resume Website...
-echo.
-echo Please open your browser and navigate to: http://localhost:8000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-python -m http.server 8000 
